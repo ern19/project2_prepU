57e516c9ce311e107d5cee05ff2b4f042f01c8f1
ern19 <aarontrammell19@gmail.com>
2017-09-30 17:17:29 -0400

    Add new/created routes for users, linked new route on layout page

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const Schema = require("../db/schema.js")
 const UserModel = Schema.UserModel
-/* GET users listing. */
+/*users Index */
 router.get('/', function(req, res, next) {
   UserModel.find({})
     .then((users) => {
@@ -15,7 +15,24 @@ router.get('/', function(req, res, next) {
       console.log(error)
     })
 });
+ //new users
+router.get("/new", (request, response) => {
+  response.render("users/new")
+})
 
+router.post("/", (request, response) => {
+  const newUser = request.body
+
+  UserModel.create(newUser)
+    .then(() => {
+      response.redirect("/")
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+//show single user
 router.get("/:userId", (request, response) => {
   const userId = request.params.userId
   UserModel.findById(userId)
@@ -29,5 +46,6 @@ router.get("/:userId", (request, response) => {
       console.log(error)
     })
 })
+
 
 module.exports = router;
