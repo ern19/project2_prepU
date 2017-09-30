c8ffcce62aa84449c688641bd7e03833169fff65
ern19 <aarontrammell19@gmail.com>
2017-09-30 16:47:58 -0400

    Added users controller, added index and show routes and views

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const Schema = require("../db/schema.js")
+const UserModel = Schema.UserModel
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  UserModel.find({})
+    .then((users) => {
+      res.render('users/index', {
+        users: users
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 });
+
+router.get("/:userId", (request, response) => {
+  const userId = request.params.userId
+  UserModel.findById(userId)
+    
+    .then((user) => {
+      response.render("users/show", {
+        user: user
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 
 module.exports = router;
