9797fd1b20f94d606a3be2fb34a84acfcdb0084c
ern19 <aarontrammell19@gmail.com>
2017-10-02 12:20:40 -0400

    Corresponding serving sizes display with ingredients

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,25 +1,22 @@
-// console.log("hello world")
+
 // //When the page loads, the two right panes should be blank
-// const recipeText = $(".recipe")
 
-// const servingNumber = $(".servingNumber")
-
-// recipeText.hide()
-
-// servingNumber.hide()
 //When you click "View Ingredients" under the recipe,
 $( ".viewIngredients" ).click(function() {
     console.log(this)
     const recipeIndex = $( this ).data("recipe")
-    console.log(recipeIndex)
+    // console.log(recipeIndex)
     $( ".recipe" ).css("display", "none")
     $(`.recipe#${recipeIndex}`).css("display", "block")
-        
-    // servingNumber.show()
+    // console.log($(`.recipe#${recipeIndex}`))
+  
  });
 
-//  $( ".viewIngredients" ).click(function() {
-
-//  })
+ $( ".viewIngredients" ).click(function() {
+    const recipeIndex = $( this ).data("recipe")
+    $(".servings").css("display", "none")
+    $(`#${recipeIndex}.servings`).css("display", "block")
+    console.log($(`#${recipeIndex}.servings`))
+ })
 //the ingredients and servings for that recipe appears in the center
 //and right panes
