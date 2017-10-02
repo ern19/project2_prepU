877d6bb148f2b6b29f96e8c2642ecf48a5a5a12b
ern19 <aarontrammell19@gmail.com>
2017-10-02 11:16:55 -0400

    recipe views functionality works as intended

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,25 @@
-$( ".recipeLinks" ).click(function() {
-    console.log( "Handler for .click() called." );
-  });
+// console.log("hello world")
+// //When the page loads, the two right panes should be blank
+// const recipeText = $(".recipe")
+
+// const servingNumber = $(".servingNumber")
+
+// recipeText.hide()
+
+// servingNumber.hide()
+//When you click "View Ingredients" under the recipe,
+$( ".viewIngredients" ).click(function() {
+    console.log(this)
+    const recipeIndex = $( this ).data("recipe")
+    console.log(recipeIndex)
+    $( ".recipe" ).css("display", "none")
+    $(`.recipe#${recipeIndex}`).css("display", "block")
+        
+    // servingNumber.show()
+ });
+
+//  $( ".viewIngredients" ).click(function() {
+
+//  })
+//the ingredients and servings for that recipe appears in the center
+//and right panes
